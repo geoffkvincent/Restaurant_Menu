e5ca34d3eb6b128a8fd7b62257a071b3ed56994d
Geoff Vincent <geoffkvincent@gmail.com>
2018-09-17 18:17:24 -0600

    added navbar and routes in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import About from './components/About'
+import Menu from './components/Menu';
+import NoMatch from './components/NoMatch'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Fragment>
+    <NavBar />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route component={NoMatch} />
+    </Switch>
+  </Fragment>
+
+);
 
 export default App;
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const styles = {
+  active: {
+    textDecoration: 'none',
+    fontWeight: 'bold',
+    color: 'black',
+  }
+}
+
+const NavBar = () => (
+  <nav>
+    <NavLink exact activeStyle={styles.active} to="/">Home</NavLink>
+    {'  /  '}
+    <NavLink exact activeStyle={styles.active} to="/about">About</NavLink>
+    {'  /  '}
+    <NavLink exact activeStyle={styles.active} to="/menu">Menu</NavLink>
+  </nav>
+)
+
+export default NavBar;
