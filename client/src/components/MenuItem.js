e88f616b59593d675ab1ad76b5acb1a0824f9e3c
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-03 12:26:12 -0700

    adding semantic

--- client/src/components/MenuItem.js
+++ client/src/components/MenuItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Form from './Form'
-import {Button, Card} from 'semantic-ui-react'
+import {Button, Card, Container} from 'semantic-ui-react'
 
 class MenuItem extends React.Component {
   state = { menu_item: {} }
@@ -25,27 +25,29 @@ class MenuItem extends React.Component {
   show() {
     const { menu_item: { name, description, price }} = this.state;
     return (
-      <Card centered>
-        <Card.Content>
-          <Card.Header>
-            <h1>{name}</h1>
-          </Card.Header>
-          <Card.Description>
-            <h3>{description}</h3>
-          </Card.Description>
-          <Card.Meta>
-            <h3>{price}</h3>
-          </Card.Meta>
-          <Card.Content extra>
-            <Button 
-              onClick={this.toggleEdit}
-              size='mini'
-            >
-              { this.state.edit ? 'Cancel' : 'Edit' }
-            </Button>
+      <Container>
+        <Card centered>
+          <Card.Content>
+            <Card.Header>
+              <h1>{name}</h1>
+            </Card.Header>
+            <Card.Description>
+              <h3>{description}</h3>
+            </Card.Description>
+            <Card.Meta>
+              <h3>{price}</h3>
+            </Card.Meta>
+            <Card.Content extra>
+              <Button 
+                onClick={this.toggleEdit}
+                size='mini'
+              >
+                { this.state.edit ? 'Cancel' : 'Edit' }
+              </Button>
+            </Card.Content>
           </Card.Content>
-        </Card.Content>
-      </Card>
+        </Card>
+      </Container>
     )
   }
 
