fd89b75a210645839d591737168bffe74a32c141
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-03 12:08:22 -0700

    adding semantic

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import 'materialize-css/dist/css/materialize.min.css';
+import 'semantic-ui-css/semantic.min.css'
 
 
 ReactDOM.render(
