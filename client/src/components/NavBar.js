34b77a0fa92713c6bb2ec171cabcefba0d3dba0c
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-03 12:04:14 -0700

    removing auth

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -9,27 +9,6 @@ const styles = {
   }
 }
 
-// const additionalLinks = (history) => {
-//   if (isAuthenticated()) {
-//     return (
-//       <span>
-//         <NavLink activeStyle={styles.active} to="/menu">Menu</NavLink>
-//         {' '}
-//         <a href="#" onClick={() => {
-//           logout()
-//           history.push("/login")
-//         }}>
-//           Logout
-//         </a>
-//       </span>
-//     )
-//   } else {
-//     return (
-//       <NavLink activeStyle={styles.active} to="/login">Login</NavLink>
-//     )
-//   }
-// }
-
 const NavBar = ({ history }) => (
   <nav>
     <NavLink exact activeStyle={styles.active} to="/">Home</NavLink>
