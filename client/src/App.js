fd9ff3ad7f4783444c69abab2b469e01963553fa
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-04 16:01:07 -0700

    adding delete item

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Login from './components/Login';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import About from './components/About';
