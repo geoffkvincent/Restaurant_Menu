8cdbca9093f682b1a69ef16b2870a6194e013bc7
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-05 15:12:29 -0700

    navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
-import {Navbar} from 'semantic-ui-react'
+import {Menu} from 'semantic-ui-react'
 
 const styles = {
   active: {
@@ -11,13 +11,17 @@ const styles = {
 }
 
 const NavBar = ({ history }) => (
-  <nav>
-    <NavLink exact activeStyle={styles.active} to="/">Home</NavLink>
-     {' '}
-    <NavLink activeStyle={styles.active} to="/about">About</NavLink>
-     {' '}
-     <NavLink activeStyle={styles.active} to="/menu">Menu</NavLink>
-  </nav>
+  <Menu>
+    <Menu.Item>
+      <NavLink exact activeStyle={styles.active} to="/">Home</NavLink>
+    </Menu.Item>
+    <Menu.Item>
+      <NavLink activeStyle={styles.active} to="/about">About</NavLink>
+    </Menu.Item>
+    <Menu.Item>
+      <NavLink activeStyle={styles.active} to="/menu">Menu</NavLink>
+    </Menu.Item>
+  </Menu>
 )
 
 export default withRouter(NavBar);
