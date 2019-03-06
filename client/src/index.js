5b01531a37edfcd7907a49d633021a344802358f
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-06 13:58:38 -0700

    menu update

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import 'materialize-css/dist/css/materialize.min.css';
 import 'semantic-ui-css/semantic.min.css'
 
 
