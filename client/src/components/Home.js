c778b8db5f6a5ee47b4cba73177b51920cc668dc
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-10 14:33:04 -0600

    homepage style

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,15 +3,14 @@ import {Header, Container, Image} from 'semantic-ui-react'
 
 const Home = () => (
   <Container textAlign='center'>
-    <div className="guy-home" >
-      <Header textAlign='center' as='h1'>Guy Ferraris 
+      <Header as='h1'>
+        Guy Ferraris 
         <br/>
         Sizzlin Steak House
       </Header>
       <div className="guy-pic">
         <Image src="https://media1.fdncms.com/orlando/imager/u/blog/6220103/guyfieri.jpg?cb=1503350375"/>
       </div>
-    </div>
   </Container>
 )
 
