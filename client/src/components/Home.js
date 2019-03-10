34ac5c560eeff93300497d6d1c827847871a1c2f
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-10 14:30:48 -0600

    homepage style

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,10 +4,10 @@ import {Header, Container} from 'semantic-ui-react'
 const Home = () => (
   <Container>
     <div className="guy-home" >
-      <h1 className="header">Guy Ferraris 
+      <Header as='h1'>Guy Ferraris 
         <br/>
         Sizzlin Steak House
-      </h1>
+      </Header>
       <div className="guy-pic">
         <img src="https://media1.fdncms.com/orlando/imager/u/blog/6220103/guyfieri.jpg?cb=1503350375"/>
       </div>
