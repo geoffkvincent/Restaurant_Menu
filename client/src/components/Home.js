cc5e36dc53c23c0c31039854c97ba911b251c921
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-10 14:29:57 -0600

    homepage style

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import {Header, Container} from 'semantic-ui-react'
 
 const Home = () => (
-  <div className="guy-home" >
-    <h1 className="header">Guy Ferraris 
-      <br/>
-      Sizzlin Steak House
-    </h1>
-    <div className="guy-pic">
-      <img src="https://media1.fdncms.com/orlando/imager/u/blog/6220103/guyfieri.jpg?cb=1503350375"/>
+  <Container>
+    <div className="guy-home" >
+      <h1 className="header">Guy Ferraris 
+        <br/>
+        Sizzlin Steak House
+      </h1>
+      <div className="guy-pic">
+        <img src="https://media1.fdncms.com/orlando/imager/u/blog/6220103/guyfieri.jpg?cb=1503350375"/>
+      </div>
     </div>
-  </div>
+  </Container>
 )
 
 export default Home
